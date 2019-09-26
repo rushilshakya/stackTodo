28878a2ad02d87b74befa304fdf0c1bbc9063d18
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-26 11:42:17 -0400

    sample secrets

---
+++ secrets.sample.js
@@ -0,0 +1,5 @@
+process.env.GOOGLE_CLIENT_ID = 'something.at.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = 'someSecret'
+process.env.DATABASE_URL = 'databaseUrl'
+process.env.PORT = 8080
+process.env.SESSION_SECRET = 'a wildly insecure secret'
