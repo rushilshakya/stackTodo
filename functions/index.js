d032b3cc17e6f9370f0f1a82c1ff8d054d48593d
Rushil Shakya <rushil.shakya@gmail.com>
2019-09-29 18:42:49 -0400

    simple twilio integration

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require('firebase-functions')
+const twilio = require('twilio')
+const config = require('./config.json')
+
+const MessagingResponse = twilio.twiml.MessagingResponse
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,6 +13,44 @@ const db = admin.firestore()
 const messaging = admin.messaging()
 const userToken =
   'dseaFBZ5-jC3TOSp3lzOrR:APA91bHAA8-jtU_kwD4xX4TvsJ4RIc-EIv09PN3IojGTXnB-k2bCpCKx_NuUQM9D1F4AOqTQ4wnuGuwy3Fqxs9ecpYE_bV1TZN9fWXyGpgdlUUlfXsuF4XDZaN6XudTsDSoFCQZ-RRJh'
+
+const projectId = process.env.GCLOUD_PROJECT
+const region = 'us-central1'
+
+exports.reply = functions.https.onRequest((req, res) => {
+  let isValid = true
+  console.log(req.body.Body)
+
+  // Only validate that requests came from Twilio when the function has been
+  // deployed to production.
+  if (process.env.NODE_ENV === 'production') {
+    isValid = twilio.validateExpressRequest(req, config.TWILIO_AUTH_TOKEN, {
+      url: `https://${region}-${projectId}.cloudfunctions.net/reply`
+    })
+  }
+
+  // Halt early if the request was not sent from Twilio
+  if (!isValid) {
+    res
+      .type('text/plain')
+      .status(403)
+      .send('Twilio Request Validation Failed.')
+      .end()
+    return
+  }
+
+  // Prepare a response to the SMS message
+  const response = new MessagingResponse()
+
+  // Add text to the response
+  response.message('Hello from Google Cloud Functions!')
+
+  // Send the response
+  res
+    .status(200)
+    .type('text/xml')
+    .end(response.toString())
+})
 
 // exports.sendMessage = functions.firestore
 //   .document('todos/{todoId}')
